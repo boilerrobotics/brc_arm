5229848899363ed79b9cd81b70f2f724618198ca
Max Michalec <mmich8749@gmail.com>
2023-01-31 19:32:26 -0500

    hw interface outline

--- brc_arm_hardware_interface/include/brc_arm_hardware_interface/brc_arm_hardware_interface.hpp
+++ brc_arm_hardware_interface/include/brc_arm_hardware_interface/brc_arm_hardware_interface.hpp
@@ -1,54 +1,56 @@
 #ifndef BRC_ARM_HARDWARE_INTERFACE
 #define BRC_ARM_HARDWARE_INTERFACE
+#include "rclcpp/rclcpp.hpp"
 #include "hardware_interface/system_interface.hpp"
-#include "mock_components/generic_system.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include <memory>
+#include <string>
 #include <vector>
 #include <iostream>
 
 namespace brc_arm_hardware_interface {
 
-using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+using hardware_interface::CallbackReturn;
+using hardware_interface::return_type;
 
-class BRCArmHWInterface : public mock_components::GenericSystem {
+class BRCArmHWInterface : public hardware_interface::SystemInterface {
 public:
-    /// Read the current state values from the actuator.
-    //return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-
-    /// Write the current command values to the actuator.
-    return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override {
-        std::cout << "return" << std::endl;
-        return return_type::OK;
-    };
-};
-
-/*class BRCArmHWInterface : public hardware_interface::SystemInterface {
-public:
-    BRCArmHWInterface();
+    RCLCPP_SHARED_PTR_DEFINITIONS(BRCArmHWInterface);
 
     // LifecycleNodeInterface
-    CallbackReturn on_configure(const State& previous_state);
-    CallbackReturn on_cleanup(const State& previous_state);
-    CallbackReturn on_shutdown(const State& previous_state);
-    CallbackReturn on_activate(const State& previous_state);
-    CallbackReturn on_deactivate(const State& previous_state);
-    CallbackReturn on_error(const State& previous_state);
+    CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state);
+    // CallbackReturn on_cleanup(const rclcpp_lifecycle::State& previous_state);
+    // CallbackReturn on_shutdown(const rclcpp_lifecycle::State& previous_state);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State& previous_state);
 
     // SystemInterface overrides
     /// Initialization of the hardware interface from data parsed from the robot's URDF.
-    CallbackReturn on_init(const HardwareInfo& hardware_info) override;
+    CallbackReturn on_init(const hardware_interface::HardwareInfo& info) override;
 
     /// Exports all state interfaces for this hardware interface.
-    std::vector<StateInterface> export_state_interfaces() override;
+    std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
     /// Exports all state interfaces for this hardware interface.
-    std::vector<CommandInterface> export_command_interfaces() override;
+    std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
     /// Read the current state values from the actuator.
     return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
     /// Write the current command values to the actuator.
     return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-}*/
+
+private:
+    // Parameters for the robot simulation
+    double hw_start_sec_;
+    double hw_stop_sec_;
+    double hw_slowdown_;
+
+    // Store the command for the simulated robot
+    std::vector<double> hw_commands_;
+    std::vector<double> hw_states_;
+};
 
 }
 
--- brc_arm_hardware_interface/src/brc_arm_hardware_interface.cpp
+++ brc_arm_hardware_interface/src/brc_arm_hardware_interface.cpp
@@ -1,57 +1,217 @@
-#include "brc_arm_hardware_interface/brc_arm_hardware_interface.hpp"
-#include "pluginlib/class_list_macros.hpp"
+#include <chrono>
+#include <cmath>
+#include <limits>
+#include <memory>
 #include <vector>
 
-/*namespace brc_arm_hardware_interface {
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "brc_arm_hardware_interface/brc_arm_hardware_interface.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace brc_arm_hardware_interface {
 
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-BRCArmHWInterface::BRCArmHWInterface() { }
+CallbackReturn BRCArmHWInterface::on_init(const hardware_interface::HardwareInfo& info) {
+  if (
+    hardware_interface::SystemInterface::on_init(info) !=
+    hardware_interface::CallbackReturn::SUCCESS)
+  {
+    return hardware_interface::CallbackReturn::ERROR;
+  }
 
-CallbackReturn BRCArmHWInterface::on_init(const HardwareInfo& hardware_info) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  hw_start_sec_ = stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
+  hw_stop_sec_ = stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
+  hw_slowdown_ = stod(info_.hardware_parameters["example_param_hw_slowdown"]);
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  for (const hardware_interface::ComponentInfo & joint : info_.joints)
+  {
+    // RRBotSystemPositionOnly has exactly one state and command interface on each joint
+    if (joint.command_interfaces.size() != 1)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+        joint.command_interfaces.size());
+      return hardware_interface::CallbackReturn::ERROR;
+    }
+
+    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+        joint.command_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+      return hardware_interface::CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces.size() != 1)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' has %zu state interface. 1 expected.", joint.name.c_str(),
+        joint.state_interfaces.size());
+      return hardware_interface::CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("RRBotSystemPositionOnlyHardware"),
+        "Joint '%s' have %s state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+      return hardware_interface::CallbackReturn::ERROR;
+    }
+  }
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+CallbackReturn BRCArmHWInterface::on_configure(const rclcpp_lifecycle::State& previous_state) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Configuring ...please wait...");
+
+  for (int i = 0; i < hw_start_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_start_sec_ - i);
+  }
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  // reset values always when configuring hardware
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    hw_states_[i] = 0;
+    hw_commands_[i] = 0;
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully configured!");
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+// CallbackReturn BRCArmHWInterface::on_cleanup(const rclcpp_lifecycle::State& previous_state) {
+//
+// }
+//
+// CallbackReturn BRCArmHWInterface::on_shutdown(const rclcpp_lifecycle::State& previous_state) {
+//
+// }
+
+CallbackReturn BRCArmHWInterface::on_activate(const rclcpp_lifecycle::State& previous_state) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Activating ...please wait...");
+
+  for (int i = 0; i < hw_start_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_start_sec_ - i);
+  }
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  // command and state should be equal when starting
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    hw_commands_[i] = hw_states_[i];
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully activated!");
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+CallbackReturn BRCArmHWInterface::on_deactivate(const rclcpp_lifecycle::State& previous_state) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Deactivating ...please wait...");
+
+  for (int i = 0; i < hw_stop_sec_; i++)
+  {
+    rclcpp::sleep_for(std::chrono::seconds(1));
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "%.1f seconds left...",
+      hw_stop_sec_ - i);
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Successfully deactivated!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+CallbackReturn BRCArmHWInterface::on_error(const rclcpp_lifecycle::State& previous_state) {
 
 }
 
-CallbackReturn BRCArmHWInterface::on_configure(const State& previous_state) {
+std::vector<hardware_interface::StateInterface> BRCArmHWInterface::export_state_interfaces() {
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++)
+  {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
+  }
 
+  return state_interfaces;
 }
 
-CallbackReturn BRCArmHWInterface::on_cleanup(const State& previous_state) {
+std::vector<hardware_interface::CommandInterface> BRCArmHWInterface::export_command_interfaces() {
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++)
+  {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+  }
 
-}
-
-CallbackReturn BRCArmHWInterface::on_shutdown(const State& previous_state) {
-
-}
-
-CallbackReturn BRCArmHWInterface::on_activate(const State& previous_state) {
-
-}
-
-CallbackReturn BRCArmHWInterface::on_deactivate(const State& previous_state) {
-
-}
-
-CallbackReturn BRCArmHWInterface::on_error(const State& previous_state) {
-
-}
-
-std::vector<StateInterface> BRCArmHWInterface::export_state_interfaces() {
-
-}
-
-std::vector<CommandInterface> BRCArmHWInterface::export_command_interfaces() {
-
+  return command_interfaces;
 }
 
 return_type BRCArmHWInterface::read(const rclcpp::Time & time, const rclcpp::Duration & period) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Reading...");
 
+  for (uint i = 0; i < hw_states_.size(); i++)
+  {
+    // Simulate RRBot's movement
+    hw_states_[i] = hw_states_[i] + (hw_commands_[i] - hw_states_[i]) / hw_slowdown_;
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Got state %.5f for joint %d!",
+      hw_states_[i], i);
+  }
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Joints successfully read!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  return hardware_interface::return_type::OK;
 }
 
 return_type BRCArmHWInterface::write(const rclcpp::Time & time, const rclcpp::Duration & period) {
+  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+  RCLCPP_INFO(rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Writing...");
 
+  for (uint i = 0; i < hw_commands_.size(); i++)
+  {
+    // Simulate sending commands to the hardware
+    RCLCPP_INFO(
+      rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Got command %.5f for joint %d!",
+      hw_commands_[i], i);
+  }
+  RCLCPP_INFO(
+    rclcpp::get_logger("RRBotSystemPositionOnlyHardware"), "Joints successfully written!");
+  // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  return hardware_interface::return_type::OK;
 }
 
-}*/
+}
 
 PLUGINLIB_EXPORT_CLASS(brc_arm_hardware_interface::BRCArmHWInterface , hardware_interface::SystemInterface)
